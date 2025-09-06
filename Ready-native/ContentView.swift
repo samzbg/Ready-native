23481b1a3a6e33b9ff214e32c02d03996e61a819
Sam Zetterberg <sam@ready.app>
2025-09-06 17:21:31 +0300

    Initial Commit

---
+++ Ready-native/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ready-native
+//
+//  Created by Samuli Zetterberg on 6.9.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ready-native/Ready_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ready_nativeApp.swift
+//  Ready-native
+//
+//  Created by Samuli Zetterberg on 6.9.2025.
+//
+
+import SwiftUI
+
+@main
+struct Ready_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
